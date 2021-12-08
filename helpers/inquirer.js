569c42a144c30e242abb0a929de1a8580ad28885
dancer1325 <alfredotic0809@gmail.com>
2021-12-09 00:44:15 +0100

    Add commentaries

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,147 +1,131 @@
-const inquirer = require('inquirer');
-require('colors');
+const inquirer = require("inquirer");
+require("colors");
 
-const preguntas = [
-    {
-        type: 'list',
-        name: 'opcion',
-        message: '¿Qué desea hacer?',
-        choices: [
-            {
-                value: 1,
-                name: `${ '1.'.green } Buscar ciudad`
-            },
-            {
-                value: 2,
-                name: `${ '2.'.green } Historial`
-            },
-            {
-                value: 0,
-                name: `${ '0.'.green } Salir`
-            },
-        ]
-    }
-];
-
-
+// Inquirer.question
+const preguntas = [{
+    type: "list",
+    name: "opcion",
+    message: "¿Qué desea hacer?",
+    choices: [{
+            value: 1,
+            name: `${"1.".green} Buscar ciudad`,
+        },
+        {
+            value: 2,
+            name: `${"2.".green} Historial`,
+        },
+        {
+            value: 0,
+            name: `${"0.".green} Salir`,
+        },
+    ],
+}, ];
 
 const inquirerMenu = async() => {
-
     console.clear();
-    console.log('=========================='.green);
-    console.log('  Seleccione una opción'.white );
-    console.log('==========================\n'.green);
+    // console.log(process.argv);
+    // console.log(process.env);
+    console.log("==========================".green);
+    console.log("  Seleccione una opción".white);
+    console.log("==========================\n".green);
 
+    // Destructure, getting just the field by 'name' with the value selected
     const { opcion } = await inquirer.prompt(preguntas);
 
     return opcion;
-}
+};
 
 const pausa = async() => {
-    
-    const question = [
-        {
-            type: 'input',
-            name: 'enter',
-            message: `Presione ${ 'enter'.green } para continuar`
-        }
-    ];
+    // Inquirer.question
+    const question = [{
+        type: "input",
+        name: "enter",
+        message: `Presione ${"enter".green} para continuar`,
+    }, ];
 
-    console.log('\n');
+    console.log("\n");
     await inquirer.prompt(question);
-}
+};
 
-const leerInput = async( message ) => {
-
-    const question = [
-        {
-            type: 'input',
-            name: 'desc',
-            message,
-            validate( value ) {
-                if( value.length === 0 ) {
-                    return 'Por favor ingrese un valor';
-                }
-                return true;
+const leerInput = async(message) => {
+    // Inquirer.question
+    const question = [{
+        type: "input",
+        name: "desc",
+        message,
+        validate(value) {
+            // Function to validate the user input
+            if (value.length === 0) {
+                return "Por favor ingrese un valor";
             }
-        }
-    ];
-
+            return true;
+        },
+    }, ];
+    // Destructure, getting just the field by 'name' with the value selected
     const { desc } = await inquirer.prompt(question);
     return desc;
-}
+};
 
-const listarLugares = async( lugares = [] ) => {
-
-    const choices = lugares.map( (lugar, i) => {
-
+// Indicate that the argument is an array to be able to map afterwards
+const listarLugares = async(lugares = []) => {
+    const choices = lugares.map((lugar, i) => {
         const idx = `${i + 1}.`.green;
 
         return {
             value: lugar.id,
-            name:  `${ idx } ${ lugar.nombre }`
-        }
+            name: `${idx} ${lugar.nombre}`,
+        };
     });
-
+    // Introduce a new element at the start of the array
     choices.unshift({
-        value: '0',
-        name: '0.'.green + ' Cancelar'
+        value: "0",
+        name: "0.".green + " Cancelar",
     });
-
-    const preguntas = [
-        {
-            type: 'list',
-            name: 'id',
-            message: 'Seleccione lugar:',
-            choices
-        }
-    ]
-
+    // Inquirer.question
+    const preguntas = [{
+        type: "list",
+        name: "id",
+        message: "Seleccione lugar:",
+        choices,
+    }, ];
+    // Destructure, getting just the field by 'name' with the value selected
     const { id } = await inquirer.prompt(preguntas);
     return id;
-}
+};
 
 const confirmar = async(message) => {
-
-    const question = [
-        {
-            type: 'confirm',
-            name: 'ok',
-            message
-        }
-    ];
-
+    // Inquirer.question
+    const question = [{
+        type: "confirm",
+        name: "ok",
+        message,
+    }, ];
+    // Destructure, getting just the field by 'name' with the value selected
     const { ok } = await inquirer.prompt(question);
     return ok;
-}   
+};
 
-const mostrarListadoChecklist = async( tareas = [] ) => {
-
-    const choices = tareas.map( (tarea, i) => {
-
+const mostrarListadoChecklist = async(tareas = []) => {
+    const choices = tareas.map((tarea, i) => {
         const idx = `${i + 1}.`.green;
 
         return {
             value: tarea.id,
-            name:  `${ idx } ${ tarea.desc }`,
-            checked: ( tarea.completadoEn ) ? true : false
-        }
+            name: `${idx} ${tarea.desc}`,
+            checked: tarea.completadoEn ? true : false,
+        };
     });
-
-    const pregunta = [
-        {
-            type: 'checkbox',
-            name: 'ids',
-            message: 'Selecciones',
-            choices
-        }
-    ]
-
+    // Inquirer.question
+    const pregunta = [{
+        type: "checkbox",
+        name: "ids",
+        message: "Selecciones",
+        choices,
+    }, ];
+    // Destructure, getting just the field by 'name' with the value selected
     const { ids } = await inquirer.prompt(pregunta);
     return ids;
-}
-
-
+};
 
 module.exports = {
     inquirerMenu,
@@ -149,5 +133,5 @@ module.exports = {
     leerInput,
     listarLugares,
     confirmar,
-    mostrarListadoChecklist
-}
+    mostrarListadoChecklist,
+};
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,133 +1,115 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const axios = require('axios');
-
+const axios = require("axios");
 
 class Busquedas {
-    
-    historial = [];
-    dbPath = './db/database.json';
+    historial = []; // Variable to retrieve and updating each time the historic places search in different sessions
+    dbPath = "./db/database.json"; // Static file to store the historic places searched
 
     constructor() {
         this.leerDB();
     }
 
     get historialCapitalizado() {
-        return this.historial.map( lugar => {
+        return this.historial.map((lugar) => {
+            let palabras = lugar.split(" ");
+            palabras = palabras.map((p) => p[0].toUpperCase() + p.substring(1));
 
-            let palabras = lugar.split(' ');
-            palabras = palabras.map( p => p[0].toUpperCase() + p.substring(1) );
-
-            return palabras.join(' ')
-
-        })
+            return palabras.join(" "); // Join all the elements of the array b " "
+        });
     }
 
-
+    // Extract request params
     get paramsMapbox() {
         return {
-            'access_token': process.env.MAPBOX_KEY,
-            'limit': 5,
-            'language': 'es'
-        }
+            access_token: process.env.MAPBOX_KEY, // Get a environment variable
+            limit: 5,
+            language: "es",
+        };
     }
 
+    // Extract common query params in the weather request
     get paramsWeather() {
         return {
             appid: process.env.OPENWEATHER_KEY,
-            units: 'metric',
-            lang: 'es'
-        }
+            units: "metric",
+            lang: "es",
+        };
     }
 
-    async ciudad( lugar = '' ) {
-
+    async ciudad(lugar = "") {
         try {
             // Petición http
+            // Create a new instance of axios
             const intance = axios.create({
-                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
-                params: this.paramsMapbox
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox,
             });
 
+            // The response is a promise
             const resp = await intance.get();
-            return resp.data.features.map( lugar => ({
+            // .features because that's the structure of the response received
+            return resp.data.features.map((lugar) => ({
                 id: lugar.id,
                 nombre: lugar.place_name,
                 lng: lugar.center[0],
                 lat: lugar.center[1],
             }));
-            
         } catch (error) {
             return [];
         }
     }
 
-
-    async climaLugar( lat, lon ) {
-
+    async climaLugar(lat, lon) {
         try {
-            
             const instance = axios.create({
                 baseURL: `https://api.openweathermap.org/data/2.5/weather`,
-                params: { ...this.paramsWeather, lat, lon }
-            })
+                params: {...this.paramsWeather, lat, lon }, // Add several params, including longigute and latitude
+            });
 
             const resp = await instance.get();
-            const { weather, main } = resp.data;
+            const { weather, main } = resp.data; // Destructurate the response body
 
             return {
                 desc: weather[0].description,
                 min: main.temp_min,
                 max: main.temp_max,
-                temp: main.temp
-            }
-
+                temp: main.temp,
+            };
         } catch (error) {
             console.log(error);
         }
-
     }
 
-
-    agregarHistorial( lugar = '' ) {
-
-        if( this.historial.includes( lugar.toLocaleLowerCase() ) ){
+    agregarHistorial(lugar = "") {
+        if (this.historial.includes(lugar.toLocaleLowerCase())) {
             return;
         }
-        this.historial = this.historial.splice(0,5);
+        this.historial = this.historial.splice(0, 5); // Remove the first 5 elements in historial
 
-        this.historial.unshift( lugar.toLocaleLowerCase() );
+        this.historial.unshift(lugar.toLocaleLowerCase()); // Add the first element of the array
 
         // Grabar en DB
         this.guardarDB();
     }
 
     guardarDB() {
-
+        // If there are several properties to store --> Wrap it into an object
         const payload = {
-            historial: this.historial
+            historial: this.historial, // Isn't it redundant?
         };
 
-        fs.writeFileSync( this.dbPath, JSON.stringify( payload ) );
-
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload));
     }
 
     leerDB() {
+        if (!fs.existsSync(this.dbPath)) return;
 
-        if( !fs.existsSync( this.dbPath ) ) return;
-        
-        const info = fs.readFileSync( this.dbPath, { encoding: 'utf-8' });
-        const data = JSON.parse( info );
+        const info = fs.readFileSync(this.dbPath, { encoding: "utf-8" });
+        const data = JSON.parse(info);
 
-        this.historial = data.historial;
-
-
+        this.historial = data.historial; // Add to the variable the previous historical cities searched
     }
-
 }
-
-
-
-
 
 module.exports = Busquedas;
